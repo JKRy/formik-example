dcf6e14ed7e4f508a487f7b4aa614ffcdf42b774
Jessica Ryan <jessica.ryan@dazn.com>
2019-07-29 12:39:23 +0100

    Add additional fields and test styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,60 +1,113 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Formik,
-  Form,
-  Field,
-  ErrorMessage,
-  FormikProps,
-  FormikActions
+    Formik,
+    Form,
+    Field,
+    ErrorMessage,
+    FormikProps,
+    FormikActions,
 } from 'formik';
 
 import * as Yup from 'yup';
 
+import styles from './App.scss';
+
 interface MyFormValues {
+    firstName: string;
+    lastName: string;
     email: string;
 }
+
+const errors = {
+    requiredField: 'This field is required',
+    lengthTooShort: 'Value is too short',
+};
 
 // TODO: add react-error-overlay
 
 const signupSchema = Yup.object().shape({
+    firstName: Yup.string()
+        .min(2, errors.lengthTooShort)
+        .required(errors.requiredField),
+    lastName: Yup.string()
+        .min(2, errors.lengthTooShort)
+        .required(errors.requiredField),
     email: Yup.string()
-      .email("valid email")
-      .required("This field is required")
+        .email('Please enter a valid email')
+        .required(errors.requiredField),
 });
 
 class App extends Component<{}, {}> {
-  render() {
-    return (
-        <>
-        <div>Correctly rendered</div>
+    render() {
+        return (
+            <>
+                <Formik
+                    initialValues={{
+                        firstName: '',
+                        lastName: '',
+                        email: '',
+                    }}
+                    onSubmit={(
+                        values: MyFormValues,
+                        actions: FormikActions<MyFormValues>
+                    ) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            actions.setSubmitting(false);
+                        }, 400);
+                    }}
+                    validationSchema={signupSchema}
+                >
+                    {(formikBag: FormikProps<MyFormValues>) => (
+                        <Form>
+                            <label
+                                className={styles.formLabel}
+                                htmlFor="firstName"
+                            >
+                                First Name
+                            </label>
+                            <Field type="firstName" name="firstName" />
+                            <ErrorMessage
+                                className={styles.errorMessage}
+                                name="firstName"
+                                component="div"
+                            />
 
-            <Formik
-                initialValues={{ email: "" }}
-                onSubmit={(
-                values: MyFormValues,
-                actions: FormikActions<MyFormValues>
-                ) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    actions.setSubmitting(false);
-                }, 400);
-                }}
-                validationSchema={signupSchema}
-            >
-                {(formikBag: FormikProps<MyFormValues>) => (
-                    <Form>
-                        <label htmlFor="email">Email</label>
-                        <Field type="email" name="email" />
-                        <ErrorMessage name="email" />
-                        <button type="submit" disabled={formikBag.isSubmitting}>
-                        Submit
-                        </button>
-                    </Form>
-                )}
-            </Formik>
-        </>
-    );
-  }
+                            <label
+                                className={styles.formLabel}
+                                htmlFor="lastName"
+                            >
+                                Last Name
+                            </label>
+                            <Field type="lastName" name="lastName" />
+                            <ErrorMessage
+                                className={styles.errorMessage}
+                                name="lastName"
+                                component="div"
+                            />
+
+                            <label className={styles.formLabel} htmlFor="email">
+                                Email
+                            </label>
+                            <Field type="email" name="email" />
+                            <ErrorMessage
+                                className={styles.errorMessage}
+                                name="email"
+                                component="div"
+                            />
+                            <div>{`Is form valid: ${formikBag.isValid}`}</div>
+                            <button
+                                type="submit"
+                                disabled={formikBag.isSubmitting}
+                            >
+                                Submit
+                            </button>
+                        </Form>
+                    )}
+                </Formik>
+            </>
+        );
+    }
 }
 
 export default App;
